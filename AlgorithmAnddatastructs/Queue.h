8a88d9ae6fe498f79ae37bfa55de2a766741a5c3
jason.howard <jason.howard@gazillion.com>
2015-08-20 13:20:11 -0700

    - Creation of NetMessage class and ExtendedNetMessage class for testing priority queue Type

--- AlgorithmAnddatastructs/Queue.h
+++ AlgorithmAnddatastructs/Queue.h
@@ -33,6 +33,49 @@ public:
 	inline bool operator () (T lVal, T rVal) { return ((*lVal) > (*rVal)); }
 };
 
+class NetMessage
+{
+public:
+	NetMessage() : m_priority(0), m_id(0) {}
+	NetMessage(int p, int id) : m_priority(p), m_id(id) {}
+	~NetMessage() {}
+
+	int GetPriority() { return m_priority; }
+	int GetID()		  { return m_id; }
+
+	//////////////////////////////////////////////////////////////////////////
+	// Overloaded Operators ////////
+	//////////////////////////////////////////////////////////////////////////
+	// Less than NetMessage compare
+	bool operator < (NetMessage& m) 
+	{
+		if (m_priority < m.GetPriority()) { return true; }
+		else if (m_id < m.GetID())		  { return true; }
+		return false;
+	}
+
+	// Greater than NetMessage compare
+	bool operator > (NetMessage& m) { return *this > m; }
+
+private:
+	int m_priority;
+	int m_id;
+};
+
+
+class ExtendedNetMessage : public NetMessage
+{
+typedef NetMessage BASE;
+public:
+	ExtendedNetMessage() : m_id(0), m_priority(0) {}
+	ExtendedNetMessage(int p, int id) : m_priority(p), m_id(id) {}
+	~ExtendedNetMessage() {}
+
+private:
+	int m_id;
+	int m_priority;
+};
+
 template<typename T>
 class Queue
 {
@@ -95,7 +138,7 @@ public:
 	int GetMaxSize()		{ return m_size; }
 	bool isEmpty()			{ return (m_container.GetSize() == 0); }
 
-	// resize the queue to store more data
+
 	void Resize(int size)	{ assert(size > 0); m_size = size; }
 
 private:
